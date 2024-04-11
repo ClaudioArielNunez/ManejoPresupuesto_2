9237b979aad65d56c23f12cf24faaa1800da6495
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-11 18:02:34 -0300

    ViewModel de Cuenta y vista de Cuenta listo

---
+++ ManejoPresupuesto_2/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto_2.Models
+{
+    public class CuentaCreacionViewModel : Cuenta
+    {
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }
+    }
+
+}
