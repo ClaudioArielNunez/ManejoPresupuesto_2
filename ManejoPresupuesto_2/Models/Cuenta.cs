7a1fd760475d1deff8aa6635cdaefaf058af4d8c
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-11 17:57:21 -0300

    Creacion de clase Cuenta con validaciones

---
+++ ManejoPresupuesto_2/Models/Cuenta.cs
@@ -0,0 +1,19 @@
+﻿using ManejoPresupuesto_2.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto_2.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength:50)]
+        [PrimeraLetraMayusculaAtribute]
+        public string Nombre { get; set; }
+        [Display(Name ="Tipo de Cuenta")]
+        public int TipoCuentaId { get; set; }
+        public decimal Balance { get; set; }
+        [StringLength(maximumLength:1000)]
+        public string Descripcion { get; set; }
+    }
+}
