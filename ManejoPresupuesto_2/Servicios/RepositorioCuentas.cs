cf35c5340b34c7bbe2e08babbe7f8db5193d2aed
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-16 21:46:04 -0300

    Creacion de metodo Buscar, query Inner Join

--- ManejoPresupuesto_2/Servicios/RepositorioCuentas.cs
+++ ManejoPresupuesto_2/Servicios/RepositorioCuentas.cs
@@ -6,6 +6,7 @@ namespace ManejoPresupuesto_2.Servicios
 {
     public interface IRepositorioCuentas
     {
+        Task<IEnumerable<Cuenta>> Buscar(int usuarioId);
         Task Crear(Cuenta cuenta);
     }
     public class RepositorioCuentas : IRepositorioCuentas
@@ -16,6 +17,8 @@ namespace ManejoPresupuesto_2.Servicios
         {
             connectionString = configuration.GetConnectionString("DefaultConnection");
         }
+
+        //METODO PARA CREAR CUENTAS
         public async Task Crear(Cuenta cuenta)
         {
             using var conexion = new SqlConnection(connectionString);
@@ -23,6 +26,17 @@ namespace ManejoPresupuesto_2.Servicios
                                                         VALUES(@Nombre, @TipoCuentaId, @Descripcion, @Balance);
                                                         SELECT SCOPE_IDENTITY();", cuenta);
             cuenta.Id = id; //Esto es mas por si queremos llevar a una pagina de detalle, donde para ubicar usariamos el id
+        }
+
+        //METDO PARA LISTAR LAS CUENTAS
+        public async Task<IEnumerable<Cuenta>> Buscar(int usuarioId)
+        {
+            using var conexion = new SqlConnection(connectionString);
+
+            return await conexion.QueryAsync<Cuenta>(@"SELECT C.Id, C.Nombre, Balance, T.Nombre as TipoCuenta
+                                                       FROM CUENTAS AS C INNER JOIN TiposCuentas AS T
+                                                       ON C.TipoCuentaId = @UsuarioId
+                                                       order by T.Orden", new {usuarioId});
         }
     }
 }
