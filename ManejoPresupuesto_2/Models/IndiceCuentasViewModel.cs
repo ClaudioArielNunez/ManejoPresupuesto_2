30f974bc8fe033ec00b93b39138cc9a7d4c14745
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-16 21:47:59 -0300

    Creacion de  viewModel para cuentas

---
+++ ManejoPresupuesto_2/Models/IndiceCuentasViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ManejoPresupuesto_2.Models
+{
+    public class IndiceCuentasViewModel
+    {
+        public string TipoCuenta { get; set; }
+        public IEnumerable<Cuenta> Cuentas { get; set;}        
+        public Decimal Balance => Cuentas.Sum(x => x.Balance);
+        //calcula la suma del Balance de todas las Cuenta en la colección Cuentas
+    }
+}
